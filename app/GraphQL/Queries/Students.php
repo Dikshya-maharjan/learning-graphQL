cff712797efaeaf578e8330f055baafa6a03a961
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-07-14 14:31:19 +0545

    feat(graphql): add students query resolver

---
+++ app/GraphQL/Queries/Students.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\GraphQL\Queries;
+use App\Models\Student;
+
+final class Students
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        return Student::all();
+    }
+}
