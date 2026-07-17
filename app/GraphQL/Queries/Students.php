58a11c11527d6e6e6f0809acda0b69738fcad015
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-07-17 15:41:00 +0545

    feat:add Students.php file

--- app/GraphQL/Queries/Students.php
+++ app/GraphQL/Queries/Students.php
@@ -8,6 +8,9 @@ final class Students
     /** @param  array{}  $args */
     public function __invoke(null $_, array $args)
     {
-        return Student::all();
+        // return Student::all();
+                return Student::all();
+
     }
+
 }
